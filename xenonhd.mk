f0ff558d8f2ac1f7f3e8c8d2158c802b5b861abe
Albert I <krascgq@outlook.co.id>
2017-12-11 10:07:14 +0800

    mido: Update build fingerprint to V9.0.5.0.NCFMIEI
    Change-Id: I935c4f27bd8820064ceb92e15dbb054c41d8ace2
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- xenonhd.mk
+++ xenonhd.mk
@@ -29,5 +29,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.xenonhd.donate="https://www.paypal.me/dadi11"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys" \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V8.5.4.0.NCFMIED release-keys"
+    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V9.0.5.0.NCFMIEI:user/release-keys" \
+    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.0.5.0.NCFMIEI release-keys"
