adf235de58de08dfd4e7e9c73b8d4edd8041f877
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-03-04 10:12:25 +0200

    mido: doze: Rename package

--- doze/src/com/custom/ambient/display/DozeSettingsActivity.java
+++ doze/src/com/custom/ambient/display/DozeSettingsActivity.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.doze;
+package com.custom.ambient.display;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
